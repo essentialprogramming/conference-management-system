f7a999f42179d1a8a96e6dda049dca3f492cb0e6
Ioana Nes <ioanapopa095@gmail.com>
2020-04-09 13:55:57 +0300

    delete mapping for event and proposal from UserEntity

--- src/main/java/com/entities/UserEntity.java
+++ src/main/java/com/entities/UserEntity.java
@@ -30,21 +30,6 @@ public class UserEntity {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @ManyToMany(mappedBy = "authors")
-    private List<ProposalEntity> proposalEntitiesAuthors;
-
-    @ManyToMany(mappedBy = "reviewers")
-    private List<ProposalEntity> proposalEntitiesReviewers;
-
-    @ManyToMany(mappedBy = "programCommittee")
-    private List<EventEntity> eventsPC;
-
-    @ManyToMany(mappedBy = "participants")
-    private List<EventEntity> eventsParticipants;
-
-    @ManyToMany(mappedBy = "speakers")
-    private List<EventEntity> eventsSpeakers;
-
     @OneToOne(mappedBy = "supervisor")
     private SectionEntity section;
 
